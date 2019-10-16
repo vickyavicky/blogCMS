23a1d107959f57e370143bd3c96aa91361f91323
Victor Asava <victor.asava@APOLLOGROUP.CO.KE>
2019-10-16 17:41:05 +0300

    added read more buttons after every blogpost

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -87,6 +87,7 @@
           <li>Maecenas sed diam eget risus varius blandit sit amet non magna.</li>
         </ol>
         <p>Cras mattis consectetur purus sit amet fermentum. Sed posuere consectetur est at lobortis.</p>
+        <a href="#" class="btn btn-primary">Read More</a>
       </div><!-- /.blog-post -->
 
       <div class="blog-post">
@@ -99,6 +100,7 @@
         </blockquote>
         <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
         <p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+        <a href="#" class="btn btn-primary">Read More</a>
       </div><!-- /.blog-post -->
 
       <div class="blog-post">
@@ -113,6 +115,7 @@
         </ul>
         <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
         <p>Donec ullamcorper nulla non metus auctor fringilla. Nulla vitae elit libero, a pharetra augue.</p>
+        <a href="#" class="btn btn-primary">Read More</a>
       </div><!-- /.blog-post -->
 
      <!-- <nav class="blog-pagination"> -->
