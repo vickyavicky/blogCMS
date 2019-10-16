b88bb59bd7961f4268b52f6feb039a527351fda3
Victor Asava <victor.asava@APOLLOGROUP.CO.KE>
2019-10-16 16:44:05 +0300

    split header footer and sidebar

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,68 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>BlogCMS</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.3/examples/blog/">
+
+    <!-- Bootstrap core CSS -->
+<link rel="stylesheet" href="{{ asset('css/bootstrap.css')}}">
+
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+    <!-- Custom styles for this template -->
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display:700,900" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link rel="stylesheet" href="{{ asset('css/blog.css')}}" >
+  </head>
+  <body>
+  <div class="container">
+  <!--   <header class="blog-header py-3">
+      <div class="row flex-nowrap justify-content-between align-items-center">
+        <div class="col-4 pt-1">
+          <a class="text-muted" href="#">Subscribe</a>
+        </div>
+        <div class="col-4 text-center">
+          <a class="blog-header-logo text-dark" href="#">Large</a>
+        </div>
+        <div class="col-4 d-flex justify-content-end align-items-center">
+          <a class="text-muted" href="#">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="mx-3" role="img" viewBox="0 0 24 24" focusable="false"><title>Search</title><circle cx="10.5" cy="10.5" r="7.5"/><path d="M21 21l-5.2-5.2"/></svg>
+          </a>
+          <a class="btn btn-sm btn-outline-secondary" href="#">Sign up</a>
+        </div>
+      </div>
+    </header> -->
+
+  <div class="nav-scroller py-1 mb-2">
+    <nav class="nav d-flex justify-content-between">
+      <a class="p-2 text-muted" href="#">Home</a>
+      <a class="p-2 text-muted" href="#">PHP</a>
+      <a class="p-2 text-muted" href="#">Pearl</a>
+      <a class="p-2 text-muted" href="#">Java</a>
+      <a class="p-2 text-muted" href="#">CSS</a>
+      <a class="p-2 text-muted" href="#">Python</a>
+      <a class="p-2 text-muted" href="#">Bootstrap</a>
+      <a class="p-2 text-muted" href="#">Laravel</a>
+      <a class="p-2 text-muted" href="#">JavaScript</a>
+      <a class="p-2 text-muted" href="#">Angular</a>
+      <a class="p-2 text-muted" href="#">Vue JS</a>
+    </nav>
+  </div>
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,61 @@
+<aside class="col-md-4 blog-sidebar">
+
+    <div class="p-4">
+        <h4 class="font-italic">Search</h4>
+        <hr>
+        <form method="get" action="results.blade.php" class="form-inline">
+          <div class="form-group">
+            <input type="text" name="search" class="form-control" id="exampleInputName2" placeholder="Search...">
+          </div>
+        </form>
+      </div>
+
+      <div class="p-4 mb-3 bg-light rounded">
+        <h4 class="font-italic">About</h4>
+        <hr>
+        <p class="mb-0">Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
+      </div>
+
+      <div class="p-4">
+        <h4 class="font-italic">Subscribe</h4>
+        <hr>
+        <form>
+          <div class="form-group">
+            <input type="text" name="name" class="form-control" id="exampleInputEmail1" placeholder="Name">
+          </div>
+          <div class="form-group">
+            <input type="email" name="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+          </div>
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+      </div>
+
+      <div class="p-4">
+        <h4 class="font-italic">Archives</h4>
+        <hr>
+        <ol class="list-unstyled mb-0">
+          <li><a href="#">March 2014</a></li>
+          <li><a href="#">February 2014</a></li>
+          <li><a href="#">January 2014</a></li>
+          <li><a href="#">December 2013</a></li>
+          <li><a href="#">November 2013</a></li>
+          <li><a href="#">October 2013</a></li>
+          <li><a href="#">September 2013</a></li>
+          <li><a href="#">August 2013</a></li>
+          <li><a href="#">July 2013</a></li>
+          <li><a href="#">June 2013</a></li>
+          <li><a href="#">May 2013</a></li>
+          <li><a href="#">April 2013</a></li>
+        </ol>
+      </div>
+
+      <div class="p-4">
+        <h4 class="font-italic">Elsewhere</h4>
+        <hr>
+        <ol class="list-unstyled">
+          <li><a href="#">GitHub</a></li>
+          <li><a href="#">Twitter</a></li>
+          <li><a href="#">Facebook</a></li>
+        </ol>
+      </div>
+    </aside><!-- /.blog-sidebar -->
